d07cddcba5cb6f1f06fa07db2221739c70a16391
Jan Špaček <patek.mail@gmail.com>
2015-12-11 13:38:24 +0100

    Render traces to png images

--- src/cjunimir/core.clj
+++ src/cjunimir/core.clj
@@ -5,15 +5,12 @@
             [clojure.java.io :as io])
   (:gen-class))
 
-(defn slurp-program [argv]
-  (let [stream (if (>= (count argv) 1)
-                 (java.io.File. (get argv 0))
-                 *in*)]
-    (with-open [input (io/reader stream)]
-      (slurp input))))
-
 (defn -main [& args]
   (let [argv (vec args)
-        program (parse/parse (slurp-program argv))
-        segments (eval/eval-program program)]
-    (render/render-svg *out* segments)))
+        input-file (java.io.File. (argv 0))
+        output-file (java.io.File. (argv 1))]
+    (with-open [input (io/reader input-file)
+                output (java.io.FileOutputStream. output-file)]
+      (let [program (parse/parse (slurp input))
+            segments (eval/eval-program program)]
+        (render/render-png output segments)))))
--- src/cjunimir/render.clj
+++ src/cjunimir/render.clj
@@ -1,8 +1,12 @@
 (ns cjunimir.render
-  (:import (java.util Formatter Locale)))
+  (:import (java.awt BasicStroke Color)
+           (java.awt.geom Line2D$Double)
+           (java.awt.image BufferedImage)
+           (java.util Formatter Locale)
+           (javax.imageio ImageIO)))
 
-(defn render-svg [output segments]
-  (let [formatter (Formatter. output Locale/US)
+(defn render-svg [out-writer segments]
+  (let [formatter (Formatter. out-writer Locale/US)
         fmt (fn [s & args] (.format formatter s (to-array args)))]
     (fmt "<svg version=\"1.1\" width=\"701\" height=\"701\"")
     (fmt " xmlns=\"http://www.w3.org/2000/svg\">\n")
@@ -14,3 +18,19 @@
       (fmt " stroke=\"rgb(%d, %d, %d)\" stroke-width=\"%d\" />\n"
                (:red pen) (:green pen) (:blue pen) (:width pen)))
     (fmt "</svg>\n")))
+
+(defn render-png [out-stream segments]
+  (let [buf-img (BufferedImage. 701 701 BufferedImage/TYPE_INT_ARGB)
+        gfx (.createGraphics buf-img)]
+    (doseq [segment segments]
+      (defn coord [kw] (+ 350 (kw segment)))
+      (let [pen (:pen segment)
+            line (Line2D$Double. (coord :x1) (coord :y1) 
+                                 (coord :x2) (coord :y2))
+            color (Color. (:red pen) (:green pen) (:blue pen))
+            stroke (BasicStroke. (:width pen))]
+        (doto gfx
+          (.setPaint color)
+          (.setStroke stroke)
+          (.draw line))))
+    (ImageIO/write buf-img "png" out-stream)))
